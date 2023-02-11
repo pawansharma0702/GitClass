5ff67526f59420ae0a7eb4b27696dfbe7af66acd
pawan.sharma0702 <pawan.sharma0702@gmail.com>
2023-02-11 07:46:45 +0530

    Sub file

---
+++ sub.java
@@ -0,0 +1,25 @@
+package com.Variable_Test;
+
+public class Variable_Test_DC {
+
+	 int B=20;
+	public static void main(String[] args) 
+	{
+		Variable_Test_DC DC= new Variable_Test_DC();
+		 int A=10;   // Local Variable
+         
+    int D=DC.B;   
+    int C= A+D;
+		 System.out.println("Value of A:  "+C);
+		 System.out.println("Value of A:  "+C);
+		 System.out.println("Value of A:  "+C);
+		 System.out.println("Value of A:  "+C);
+		  
+	}
+
+	
+	
+	
+	
+	
+}
